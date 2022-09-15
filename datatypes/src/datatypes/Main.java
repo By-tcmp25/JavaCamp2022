92ea0ace2e8876e86a1c1d3ac5fa794f3fbcf647
Mehmet_pekince <83479057+By-tcmp25@users.noreply.github.com>
2022-09-15 18:43:39 +0300

    Add files via upload

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,21 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int num = 17; // tam sayılar yazılır .
+		
+		byte num2 = 20; // byte 127 den fazla sayı alamaz.
+		
+		boolean tORf = false; 		
+		
+		System.out.println(num);
+		System.out.println(num2);
+		System.out.println(tORf);
+		
+		
+
+	}
+
+}
