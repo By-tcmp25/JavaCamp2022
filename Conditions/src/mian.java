c3804b886f791bcf3edd7cbe91c10bbb7b199b89
Mehmet_pekince <83479057+By-tcmp25@users.noreply.github.com>
2022-09-15 19:00:15 +0300

    Add files via upload

---
+++ Conditions/src/mian.java
@@ -0,0 +1,25 @@
+
+public class mian {
+
+	public static void main(String[] args) {
+		
+		
+		int number = 17;
+		int inpt = 53;
+		if (number == inpt ) {
+			System.out.println("equal");
+			
+		}
+		else if (number < inpt ) {
+			
+			System.out.println("small");
+			
+		} 
+		
+		else if (number > inpt) {
+			System.out.println("big");
+			
+		}
+	}
+
+}
