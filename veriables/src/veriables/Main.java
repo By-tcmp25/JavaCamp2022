77c0c7a6b33ff1f0d3da00f5b0577355d8424d4f
Mehmet_pekince <83479057+By-tcmp25@users.noreply.github.com>
2022-09-15 18:19:23 +0300

    Add files via upload

---
+++ veriables/src/veriables/Main.java
@@ -0,0 +1,17 @@
+package veriables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int numStudents = 12;
+		String Numbers = "number of students";
+		
+		
+		System.out.println(Numbers + " = " + numStudents);
+		System.out.println("students numbers = " + numStudents);
+		
+
+	}
+
+}
