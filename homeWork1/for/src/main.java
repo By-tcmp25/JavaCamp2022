13e35d23ed83ca2e950390acedf7bc0d931dc0b9
Mehmet_pekince <83479057+By-tcmp25@users.noreply.github.com>
2022-09-19 21:55:22 +0300

    Add files via upload

---
+++ homeWork1/for/src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	for(int i = 0; i < 10; i++) {
+			System.out.println(i);
+			
+		}
+		
+		System.out.println("döngü bitmiştir...");
+		
+	}
+
+}
