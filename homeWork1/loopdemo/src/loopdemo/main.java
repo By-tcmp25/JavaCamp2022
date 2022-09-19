c61e29ef597b15ca4e8902c29535344ad40d1255
Mehmet_pekince <83479057+By-tcmp25@users.noreply.github.com>
2022-09-19 21:54:51 +0300

    Add files via upload

---
+++ homeWork1/loopdemo/src/loopdemo/main.java
@@ -0,0 +1,19 @@
+package loopdemo;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		while (i<10) {
+	
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("while döngüsü bitti...");
+		
+		
+	}
+
+}
