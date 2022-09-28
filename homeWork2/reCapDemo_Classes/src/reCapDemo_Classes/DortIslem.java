57329bf02146a5870581373e0dd2eadfea5ba81c
Mehmet_pekince <83479057+By-tcmp25@users.noreply.github.com>
2022-09-28 19:44:08 +0300

    Add files via upload

---
+++ homeWork2/reCapDemo_Classes/src/reCapDemo_Classes/DortIslem.java
@@ -0,0 +1,22 @@
+package reCapDemo_Classes;
+
+public class DortIslem {
+
+	public int topla(int sayı1, int sayı2) {
+		
+		return sayı1+sayı2;
+	}
+	
+	public int ckar(int sayı1, int sayı2) {
+		
+		return sayı1-sayı2;
+	}
+	public int carp(int sayı1, int sayı2) {
+		
+		return sayı1*sayı2;
+	}
+	public int bol(int sayı1, int sayı2) {
+		
+		return sayı1/sayı2;
+	}
+}
---
+++ homeWork2/reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,11 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		DortIslem dortislem = new DortIslem();
+		System.out.println(dortislem.carp(1,5));
+	}
+
+}
