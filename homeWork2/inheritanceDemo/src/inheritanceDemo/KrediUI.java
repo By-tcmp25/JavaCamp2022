22f3d8bde2b952b1913b37f81cc0c9474a6bceaf
Mehmet_pekince <83479057+By-tcmp25@users.noreply.github.com>
2022-10-05 20:44:08 +0300

    Add files via upload

---
+++ homeWork2/inheritanceDemo/src/inheritanceDemo/KrediUI.java
@@ -0,0 +1,8 @@
+package inheritanceDemo;
+
+public class KrediUI {
+	public void krediHesapla(BaseKrediManager BaseKrediManager) {
+		BaseKrediManager.hesapla();
+		
+	}
+}
---
+++ homeWork2/inheritanceDemo/src/inheritanceDemo/Main.java
@@ -0,0 +1,11 @@
+package inheritanceDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		KrediUI KrediUI = new KrediUI();
+		KrediUI.krediHesapla(new BaseKrediManager());
+	}
+
+}
